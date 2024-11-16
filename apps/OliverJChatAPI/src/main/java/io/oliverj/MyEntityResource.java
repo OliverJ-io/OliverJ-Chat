fe8b7dd189b0aace62a2f7e897c60abbd222c497
olliejohnson <oliver.joseph.johnson@gmail.com>
2024-11-16 13:51:29 -0500

    Added Backend

---
+++ apps/OliverJChatAPI/src/main/java/io/oliverj/MyEntityResource.java
@@ -0,0 +1,6 @@
+package io.oliverj;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
